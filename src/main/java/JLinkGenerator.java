5523b96b535d63e6e89ed90a9f9bf8565f1961d0
galba <galba@jfrog.com>
2018-11-14 23:38:52 +0200

    push to bintray

--- src/main/java/JLinkGenerator.java
+++ src/main/java/JLinkGenerator.java
@@ -1,9 +1,11 @@
+import com.intellij.notification.*;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 
@@ -13,9 +15,17 @@ import java.awt.datatransfer.StringSelection;
 
 public class JLinkGenerator extends AnAction {
 
+
+    private static final NotificationGroup BALLOON_NOTIFIER =
+            new NotificationGroup("JLink", NotificationDisplayType.BALLOON, false);
+
+
     @Override
     public void actionPerformed(AnActionEvent e) {
         String relativePathToFile = getRelativePathToFile(e);
+        if (relativePathToFile == null) {
+            return;
+        }
         CaretCoordinates caretCoordinates = getFileCoordinates(e);
 
         String jLink = generateJLink(relativePathToFile, caretCoordinates);
@@ -36,14 +46,19 @@ public class JLinkGenerator extends AnAction {
             return null;
         }
 
-        int line = caretModel.getPrimaryCaret().getVisualPosition().line;
-        int col = caretModel.getPrimaryCaret().getVisualPosition().column;
+        int line = caretModel.getPrimaryCaret().getLogicalPosition().line + 1;
+        int col = caretModel.getPrimaryCaret().getLogicalPosition().column + 1;
         return new CaretCoordinates(line, col);
     }
 
     private String getRelativePathToFile(AnActionEvent e) {
         VirtualFile virtualFile = e.getData(LangDataKeys.PSI_FILE).getVirtualFile();
         VirtualFile contentRootForFile = ProjectFileIndex.SERVICE.getInstance(e.getProject()).getContentRootForFile(virtualFile);
+        if (contentRootForFile == null) {
+            Notification notification = BALLOON_NOTIFIER.createNotification("JLink: Not supported operation", "Only Project Files Are Currently Supported", NotificationType.WARNING, null);
+            Notifications.Bus.notify(notification);
+            return null;
+        }
         return VfsUtilCore.getRelativePath(virtualFile, contentRootForFile);
     }
 
