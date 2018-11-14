8178c4538dbb79e7d4fa1755e00164041f6c1ed8
Yoaz Menda <yoazm@jfrog.com>
2018-11-14 17:21:10 +0200

    added line and column extraction

--- src/main/java/CaretCoordinates.java
+++ src/main/java/CaretCoordinates.java
@@ -1,3 +1,4 @@
+
 public class CaretCoordinates {
 
     private int line;
--- src/main/java/JLinkGenerator.java
+++ src/main/java/JLinkGenerator.java
@@ -2,26 +2,26 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.editor.CaretModel;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 
 public class JLinkGenerator extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        Project project = e.getProject();
-        Messages.showMessageDialog(project, "Hello world!", "Greeting", Messages.getInformationIcon());
-
         String relativePathToFile = getRelativePathToFile(e);
         CaretCoordinates caretCoordinates = getFileCoordinates(e);
 
         String jLink = generateJLink(relativePathToFile, caretCoordinates);
-        String[] options = new String[0];
-        Messages.showDialog(jLink, "JLink", options, 0, null);
+        StringSelection selection = new StringSelection(jLink);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(selection, selection);
     }
 
     private String generateJLink(String relativePathToFile, CaretCoordinates caretCoordinates) {
-        return "Relative path: " + relativePathToFile + " Coordinates: " + caretCoordinates;
+        return "http://localhost:63342/api/file/" + relativePathToFile + caretCoordinates;
     }
 
     private CaretCoordinates getFileCoordinates(AnActionEvent e) {
