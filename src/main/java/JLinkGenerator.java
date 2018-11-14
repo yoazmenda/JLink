165a40167e4cbd2cb684199de4be9ce900093aee
Yoaz Menda <yoazm@jfrog.com>
2018-11-14 17:53:03 +0200

    added delimiter for Jlink creation

--- src/main/java/JLinkGenerator.java
+++ src/main/java/JLinkGenerator.java
@@ -1,11 +1,18 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.impl.EditorImpl;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class JLinkGenerator extends AnAction {
 
@@ -21,7 +28,7 @@ public class JLinkGenerator extends AnAction {
     }
 
     private String generateJLink(String relativePathToFile, CaretCoordinates caretCoordinates) {
-        return "http://localhost:63342/api/file/" + relativePathToFile + caretCoordinates;
+        return "http://localhost:63342/api/file/" + relativePathToFile + ":" + caretCoordinates;
     }
 
     private CaretCoordinates getFileCoordinates(AnActionEvent e) {
@@ -38,6 +45,17 @@ public class JLinkGenerator extends AnAction {
     }
 
     private String getRelativePathToFile(AnActionEvent e) {
-        return "Relative Path";
+        final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        Path absolutePath = Paths.get(((EditorImpl) editor).getVirtualFile().getPath());
+        Project project = e.getProject();
+        if (project == null || e.getProject().getBasePath() == null) {
+            Messages.showMessageDialog(project, "", "Can't Retrieve Project Path", Messages.getWarningIcon());
+        }
+        return Paths.get(e.getProject().getBasePath()).relativize(absolutePath).toString();
+    }
+
+    @Override
+    public boolean isDumbAware() {
+        return true;
     }
 }
